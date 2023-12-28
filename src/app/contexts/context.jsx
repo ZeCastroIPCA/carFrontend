88c2e65ad50bad6bdc323404b3f9ed1e5e2a8506
Zé Castro <a20351@alunos.ipca.pt>
2023-12-28 20:24:07 +0000

    api link updated

--- src/app/contexts/context.jsx
+++ src/app/contexts/context.jsx
@@ -2,8 +2,8 @@ import { createContext } from 'react';
 
 export const APIContext = createContext();
 const ContextProvider = (props) => {
-  const api = 'http://localhost:5213/api/cars/';
-  //const api = "https://fuxiadefeitosbackend-production.up.railway.app"
+  //const api = 'http://localhost:5213/api/cars/';
+  const api = "https://isicarapi.azurewebsites.net/api/cars/";
   const exchangeApiKey = "fca_live_vtIHfJaCOjgkHCVXlWdVeGWL7KA4E2bVKIHAk671";
   const exchangeApi = "https://api.freecurrencyapi.com/v1/latest?apikey=" + exchangeApiKey + "&currencies=EUR&base_currency=USD";
   const headersCRUD = {
